2d020f77cf085ae8aa2d1f5ef955dfa990b56d52
Pavel Kubik <pavel.kubik@citrix.com>
2020-12-18 10:00:06 +0100

    Day 18 part 2

--- src/main/java/cz/pk/adventofcode/current/Day18.java
+++ src/main/java/cz/pk/adventofcode/current/Day18.java
@@ -12,19 +12,29 @@ import lombok.Data;
 @Data
 public class Day18 {
 
-    public long evaluateWithoutBrackets(String example) {
-        return evaluateWithoutBrackets(Arrays.asList(example.split(" ")));
+    public long evaluateWithoutBrackets(String example, int part) {
+        if (part == 1) {
+            return evaluateWithoutBrackets(Arrays.asList(example.split(" ")), part);
+        } else {
+            String[] subExamples = example.split("\\*");
+            long mul = 1;
+            for (String subExample : subExamples) {
+                System.out.println(subExample.trim());
+                mul *= evaluateWithoutBrackets(Arrays.asList(subExample.trim().split(" ")), part);
+            }
+            return mul;
+        }
     }
 
-    public long evaluateWithoutBrackets(List<String> parts) {
+    public long evaluateWithoutBrackets(List<String> parts, int part) {
         if (parts.size() == 1) {
             return Long.valueOf(parts.get(0));
         } else {
             long base = Long.valueOf(parts.get(parts.size() - 1));
             if (parts.get(parts.size() - 2).equals("+")) {
-                return base + evaluateWithoutBrackets(parts.subList(0, parts.size() - 2));
+                return base + evaluateWithoutBrackets(parts.subList(0, parts.size() - 2), part);
             } else if (parts.get(parts.size() - 2).equals("*")) {
-                return base * evaluateWithoutBrackets(parts.subList(0, parts.size() - 2));
+                return base * evaluateWithoutBrackets(parts.subList(0, parts.size() - 2), part);
             } else {
                 assert false;
                 return 0;
@@ -32,18 +42,18 @@ public class Day18 {
         }
     }
 
-    public long evaluateWithBrackets(String example) {
+    public long evaluateWithBrackets(String example, int part) {
         System.out.println(example);
         int firstBracket = example.indexOf("(");
         if (firstBracket == -1) {
-            return evaluateWithoutBrackets(example);
+            return evaluateWithoutBrackets(example, part);
         } else {
             // replace brackets and simplify
             int deep = 1;
             boolean containsParenthesis = false;
             StringBuilder subExample = new StringBuilder();
             int matchBracket = -1;
-            for (int i = firstBracket+1; i < example.length(); i++) {
+            for (int i = firstBracket + 1; i < example.length(); i++) {
                 if (example.charAt(i) == '(') {
                     containsParenthesis = true;
                     deep++;
@@ -61,17 +71,17 @@ public class Day18 {
             String middlePart = subExample.toString();
             String lastPart = matchBracket + 1 < example.length() ? example.substring(matchBracket + 1, example.length()) : "";
             long middleNumber = containsParenthesis ?
-                    evaluateWithBrackets(middlePart) : evaluateWithoutBrackets(middlePart);
-            return evaluateWithBrackets(firstPart + middleNumber + lastPart);
+                    evaluateWithBrackets(middlePart, part) : evaluateWithoutBrackets(middlePart, part);
+            return evaluateWithBrackets(firstPart + middleNumber + lastPart, part);
         }
     }
 
-    public long solve(String file) throws IOException {
+    public long solve(String file, int part) throws IOException {
         URL resource = getClass().getClassLoader().getResource(file);
         List<String> lines = Files.readAllLines(Path.of(resource.getPath()));
         long sum = 0;
         for (String line : lines) {
-            sum += evaluateWithBrackets(line);
+            sum += evaluateWithBrackets(line, part);
         }
         return sum;
     }
@@ -79,41 +89,38 @@ public class Day18 {
     public static void main(String[] args) throws IOException {
         long count;
         //*
-        count = new Day18().evaluateWithoutBrackets("1 + 2 * 3 + 4 * 5 + 6");
+        count = new Day18().evaluateWithoutBrackets("1 + 2 * 3 + 4 * 5 + 6", 1);
         System.out.println("Result: " + count);
         assert count == 71;
 
-        count = new Day18().evaluateWithBrackets("2 * 3 + (4 * 5)");
+        count = new Day18().evaluateWithBrackets("2 * 3 + (4 * 5)", 1);
         System.out.println("Result: " + count);
         assert count == 26;
 
-        count = new Day18().evaluateWithBrackets("5 + (8 * 3 + 9 + 3 * 4 * 3)");
+        count = new Day18().evaluateWithBrackets("5 + (8 * 3 + 9 + 3 * 4 * 3)", 1);
         System.out.println("Result: " + count);
         assert count == 437;
 
-        count = new Day18().evaluateWithBrackets("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))");
+        count = new Day18().evaluateWithBrackets("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))", 1);
         System.out.println("Result: " + count);
         assert count == 12240;
 
-        count = new Day18().evaluateWithBrackets("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2");
+        count = new Day18().evaluateWithBrackets("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2", 1);
         System.out.println("Result: " + count);
         assert count == 13632;
 
-        count = new Day18().solve("day18.txt");
+        count = new Day18().solve("day18.txt", 1);
         System.out.println("Result: " + count);
-        //assert count == 845;
+        assert count == 75592527415659l;
 
-        /*/
-
-        count = new Day13(true).solve2("2020/day12_test.txt");
+        count = new Day18().evaluateWithoutBrackets("1 + 2 * 3 + 4 * 5 + 6", 2);
         System.out.println("Result: " + count);
-        assert count == 25;
+        assert count == 231;
 
-        count = new Day13(true).solve2("2020/day12.txt");
+        count = new Day18().solve("day18.txt", 2);
         System.out.println("Result: " + count);
         //assert count == 27016;
 
-         */
         //*/
     }
 }
