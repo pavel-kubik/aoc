847d488e73f0d881734686626c428101342cf452
Pavel Kubik <pavel.kubik@citrix.com>
2020-12-19 15:23:11 +0100

    Day 19 part 1 - without ANTLR

--- src/main/java/cz/pk/adventofcode/y2020/day19/Day19.java
+++ src/main/java/cz/pk/adventofcode/y2020/day19/Day19.java
@@ -4,124 +4,141 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.BitSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
-import org.antlr.v4.runtime.ANTLRErrorListener;
-import org.antlr.v4.runtime.ANTLRInputStream;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.Parser;
-import org.antlr.v4.runtime.ParserRuleContext;
-import org.antlr.v4.runtime.RecognitionException;
-import org.antlr.v4.runtime.Recognizer;
-import org.antlr.v4.runtime.atn.ATNConfigSet;
-import org.antlr.v4.runtime.dfa.DFA;
-import org.antlr.v4.runtime.tree.ErrorNode;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.tree.ParseTreeListener;
-import org.antlr.v4.runtime.tree.ParseTreeWalker;
-import org.antlr.v4.runtime.tree.TerminalNode;
+public class Day19 {
 
-import cz.pk.adventofcode.y2020.day19.genTest.GTOutLexer;
-import cz.pk.adventofcode.y2020.day19.genTest.GTOutParser;
-import lombok.Data;
+    Map<Integer, Rule> rules;
+    Map<Integer, Set<String>> ruleCache = new HashMap<>();
 
-@Data
-public class Day19 implements ANTLRErrorListener {
+    static class Rule {
+        int id;
+        String finalText;
+        List<List<Integer>> rules;
 
-    int error;
-    int sum;
+        @Override
+        public String toString() {
+            if (finalText != null) {
+                return new StringBuilder()
+                        .append("\"")
+                        .append(finalText)
+                        .append("\"")
+                        .toString();
+            } else {
+                StringBuilder out = new StringBuilder();
+                for (List<Integer> rule : rules) {
+                    for (Integer part : rule) {
+                        out.append(" ").append(part);
+                    }
+                    out.append(" |");
+                }
+                return out.toString();
+            }
+        }
+    }
 
-    public long solve(String file) throws IOException, InterruptedException {
+    private List<String> loadFile(String file) throws IOException {
         URL resource = getClass().getClassLoader().getResource(file);
         List<String> data = Files.readAllLines(Path.of(resource.getPath()));
+        return data;
+    }
 
-        sum = 0;
-        for (String line : data) {
-            System.out.println(line);
-            error = 0;
-            ANTLRInputStream input = new ANTLRInputStream(line);
+    public Map<Integer, Rule> processGrammar(String file) throws IOException {
+        Map<Integer, Rule> out = new HashMap<>();
+        List<String> lines = loadFile(file);
+        for (String line : lines) {
+            String[] rule = line.split(":");
+            assert rule.length == 2;
+            int ruleId = Integer.valueOf(rule[0]);
+            Rule myRule = new Rule();
+            myRule.id = ruleId;
+            List<List<Integer>> rulesOr = new ArrayList<>();
+            for (String or : rule[1].trim().split("\\|")) {
+                List<Integer> rulesAnd = new ArrayList<>();
+                for (String match : or.trim().split(" ")) {
+                    if (match.startsWith("\"")) {
+                        myRule.finalText = match.replace("\"", "");
+                    } else {
+                        rulesAnd.add(Integer.valueOf(match));
+                    }
+                }
+                rulesOr.add(rulesAnd);
+            }
+            if (myRule.finalText == null) {
+                myRule.rules = rulesOr;
+            }
+            out.put(ruleId, myRule);
+        }
+        return out;
+    }
 
-            GTOutLexer lexer = new GTOutLexer(input);
-            //lexer.removeErrorListeners();
-            lexer.addErrorListener(this);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            //tokens.fill();
-            GTOutParser parser = new GTOutParser(tokens);
-            //parser.removeErrorListeners();
-            parser.addErrorListener(this);
-            ParseTree tree = parser.rule0();
+    private Set<String> expandRule(Rule rule) {
+        if (rule.finalText != null) {
+            return Set.of(rule.finalText);
+        } else if (ruleCache.containsKey(rule.id)) {
+            return ruleCache.get(rule.id);
+        } else {
+            Set<String> words = new HashSet<>();
+            for (List<Integer> cond : rule.rules) {
 
-            ParseTreeWalker walker = new ParseTreeWalker();
-            walker.walk(new ParseTreeListener() {
-                @Override
-                public void visitTerminal(TerminalNode terminalNode) {
+                Set<String> items = new HashSet<>();
+                for (Integer item : cond) {
+                    Set<String> lowWords = expandRule(rules.get(item));
+                    if (items.isEmpty()) {
+                        items.addAll(lowWords);
+                    } else {
+                        Set<String> kartezianWords = new HashSet<>();
+                        for (String prefix : items) {
+                            for (String lowWord : lowWords) {
+                                kartezianWords.add(prefix + lowWord);
+                            }
+                        }
+                        items = kartezianWords;
+                    }
                 }
-                @Override
-                public void visitErrorNode(ErrorNode errorNode) {
-                    error++;
-                }
-                @Override
-                public void enterEveryRule(ParserRuleContext parserRuleContext) {
-                }
-                @Override
-                public void exitEveryRule(ParserRuleContext parserRuleContext) {
-                }
-            }, tree);
 
-            //Thread.sleep(500);
-            if (error == 0) {
+                words.addAll(items);
+            }
+            //ruleCache.put(rule.id, words);
+            return words;
+        }
+    }
+
+    public long count(String grammar, String file) throws IOException {
+        rules = processGrammar(grammar);
+        for (int i = 0; i < rules.size(); i++) {
+            System.out.println(i + ": " + rules.get(i));
+        }
+
+        // generate all words from grammar
+        Set<String> words = expandRule(rules.get(0));
+
+        // check matching words
+        List<String> puzzleInput = loadFile(file);
+        int sum = 0;
+        for (String input : puzzleInput) {
+            if (words.contains(input)) {
                 sum++;
             }
         }
+
         return sum;
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         long count;
-        //*
-//        count = new Day19().solve("day19_test.txt");
-//        System.out.println("Result: " + count);
-//        assert count == 2;
 
-        count = new Day19().solve("day19.txt");
+        count = new Day19().count("grammar_test.txt", "day19_test.txt");
         System.out.println("Result: " + count);
-        //assert count == 845;
-        // low 103
-        // high 156 hmm 157 (?) WTF
-        // not 125 :D
+        assert count == 2;
 
-        /*/
-
-        count = new Day13(true).solve2("2020/day12_test.txt");
+        count = new Day19().count("grammar.txt", "day19.txt");
         System.out.println("Result: " + count);
-        assert count == 25;
-
-        count = new Day13(true).solve2("2020/day12.txt");
-        System.out.println("Result: " + count);
-        //assert count == 27016;
-
-         */
-        //*/
-    }
-
-    @Override
-    public void syntaxError(Recognizer<?, ?> recognizer, Object o, int i, int i1, String s, RecognitionException e) {
-        error++;
-    }
-
-    @Override
-    public void reportAmbiguity(Parser parser, DFA dfa, int i, int i1, BitSet bitSet, ATNConfigSet atnConfigSet) {
-        error++;
-    }
-
-    @Override
-    public void reportAttemptingFullContext(Parser parser, DFA dfa, int i, int i1, ATNConfigSet atnConfigSet) {
-        error++;
-    }
-
-    @Override
-    public void reportContextSensitivity(Parser parser, DFA dfa, int i, int i1, ATNConfigSet atnConfigSet) {
-        error++;
+        //assert count == 2;
     }
 }
