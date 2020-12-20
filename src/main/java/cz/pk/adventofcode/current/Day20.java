736ce50873472d0682964330818aa45259931892
Pavel Kubik <pavel.kubik@citrix.com>
2020-12-20 14:07:03 +0100

    Day20 part 2 - before rotate and find monster

--- src/main/java/cz/pk/adventofcode/current/Day20.java
+++ src/main/java/cz/pk/adventofcode/current/Day20.java
@@ -2,6 +2,7 @@ package cz.pk.adventofcode.current;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -20,8 +21,8 @@ public class Day20 {
     private Map<Integer, List<Integer>> matches;
     private static final String monster =
             "                  # \n" +
-            "#    ##    ##    ###\n" +
-            " #  #  #  #  #  #   ";
+                    "#    ##    ##    ###\n" +
+                    " #  #  #  #  #  #   ";
 
     @Data
     @AllArgsConstructor
@@ -36,15 +37,21 @@ public class Day20 {
             StringBuilder out = new StringBuilder()
                     .append("id: ")
                     .append(id)
-                    .append("\n");
-            for (int i = 0; i < pic.length; i++) {
-                for (int j = 0; j < pic.length; j++) {
-                    out.append(pic[i][j] == 1 ? "#" : ".");
-                }
-                out.append("\n");
-            }
+                    .append("\n")
+                    .append(toPic(pic));
             return out.toString();
         }
+    }
+
+    private String toPic(Integer[][] pic) {
+        StringBuilder out = new StringBuilder();
+        for (int i = 0; i < pic.length; i++) {
+            for (int j = 0; j < pic[i].length; j++) {
+                out.append(Integer.valueOf(1).equals(pic[i][j]) ? "#" : ".");
+            }
+            out.append("\n");
+        }
+        return out.toString();
     }
 
     private long extractRow(int line, boolean reverse, Integer[][] pic) {
@@ -113,6 +120,23 @@ public class Day20 {
         matches.computeIfAbsent(id, k -> new ArrayList<>()).add(id2);
     }
 
+    private void unMatch(int id) {
+        for (Map.Entry<Integer, List<Integer>> entry : matches.entrySet()) {
+            entry.getValue().remove((Integer) id);
+        }
+    }
+
+    private int commonMatch(Collection<Integer> aSet, Collection<Integer> bSet) {
+        for (Integer a : aSet) {
+            for (Integer b : bSet) {
+                if (a.equals(b)) {
+                    return a;
+                }
+            }
+        }
+        throw new InternalError("No match");
+    }
+
     public long solve(String file, int part) throws IOException {
         List<Image> images = new ImageCollector(file).processGroups();
         System.out.println(images);
@@ -131,12 +155,21 @@ public class Day20 {
                 }
             }
         }
+        System.out.println(matches);
 
         long mul = 1;
+        List<Integer> corners = new ArrayList<>();
         for (Map.Entry<Integer, List<Integer>> entry : matches.entrySet()) {
             if (entry.getValue().size() == 2) {
-                System.out.println(entry.getKey());
+                //System.out.println(entry.getKey());
+                corners.add(entry.getKey());
                 mul *= entry.getKey();
+            }
+        }
+        List<Integer> sides = new ArrayList<>();
+        for (Map.Entry<Integer, List<Integer>> entry : matches.entrySet()) {
+            if (entry.getValue().size() == 3) {
+                sides.add(entry.getKey());
             }
         }
 
@@ -144,9 +177,68 @@ public class Day20 {
             return mul;
         } else {
             // part 2 - fuck, do match
-            int waterTiles = 0;
-            
-            return waterTiles;
+            int size = (int) Math.round(Math.sqrt(images.size()));
+            int[][] order = new int[size][size];
+            order[0][0] = corners.get(0);
+
+            Set<Integer> sideMatches = new HashSet<>();
+            sideMatches.addAll(corners);
+            sideMatches.addAll(sides);
+            sideMatches.remove(order[0][0]);
+
+            unMatch(order[0][0]);
+
+            // first line
+            for (int i = 1; i < order[0].length; i++) {
+                order[0][i] = commonMatch(matches.get(order[0][i - 1]), sideMatches);
+                unMatch(order[0][i]);
+                sideMatches.remove((Integer) order[0][i]);
+            }
+            // first col
+            for (int i = 1; i < order[0].length; i++) {
+                order[i][0] = commonMatch(matches.get(order[i - 1][0]), sideMatches);
+                unMatch(order[i][0]);
+                sideMatches.remove((Integer) order[0][i]);
+            }
+            // rest of image
+            for (int i = 1; i < order.length; i++) {
+                for (int j = 1; j < order[i].length; j++) {
+                    int match = commonMatch(matches.get(order[i - 1][j]), matches.get(order[i][j - 1]));
+                    order[i][j] = match;
+                    unMatch(match);
+                }
+            }
+
+            Integer[][] finalImage = new Integer[8 * size][8 * size];
+            for (int i = 0; i < size; i++) {
+                for (int j = 0; j < size; j++) {
+                    final int imgId = order[i][j];
+                    Integer[][] img = images.stream()
+                            .filter(im -> im.id == imgId)
+                            .findFirst()
+                            .get()
+                            .pic;
+
+                    //TODO orientation - flip/rotate...
+
+                    placeImage(finalImage, img, i * 8, j * 8);
+                }
+            }
+
+            System.out.println(toPic(finalImage));
+
+            int waves = 0;
+
+
+            return waves;
+        }
+    }
+
+    private void placeImage(Integer[][] out, Integer[][] pic, int x, int y) {
+        for (int i = 1; i < 9; i++) {
+            for (int j = 1; j < 9; j++) {
+                out[x + i - 1][y + j - 1] = pic[i][j];
+            }
         }
     }
 
@@ -158,7 +250,7 @@ public class Day20 {
 
     public static void main(String[] args) throws IOException {
         long count;
-        //*
+        /*
         count = new Day20(true).solve("day20_test.txt", 1);    //9 tiles
         System.out.println("Result: " + count);
         assert count == 20899048083289l;
@@ -166,13 +258,14 @@ public class Day20 {
         count = new Day20(true).solve("day20.txt", 1); //144 tiles
         System.out.println("Result: " + count);
         assert count == 32287787075651l;
+        /*/
 
         count = new Day20(true).solve("day20_test.txt", 2);    //9 tiles
         System.out.println("Result: " + count);
         assert count == 273;
 
-        count = new Day20(true).solve("day20.txt", 2); //144 tiles
-        System.out.println("Result: " + count);
+        //        count = new Day20(true).solve("day20.txt", 2); //144 tiles
+        //        System.out.println("Result: " + count);
         //assert count == 273;
         //*/
     }
