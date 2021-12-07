74720cdbe1f023b023fc85e83c3e096e21d94896
Pavel <roigoci@gmail.com>
2021-12-07 06:30:59 +0100

    Prepare Day 8

--- src/main/java/cz/pk/adventofcode/current/Day8.java
+++ src/main/java/cz/pk/adventofcode/current/Day8.java
@@ -1,22 +1,20 @@
 package cz.pk.adventofcode.current;
 
+import cz.pk.adventofcode.util.DataCollector;
+import cz.pk.adventofcode.util.StringCollector;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import cz.pk.adventofcode.util.DataCollector;
-import cz.pk.adventofcode.util.Matrix;
-import cz.pk.adventofcode.util.StringCollector;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-
-import static cz.pk.adventofcode.util.DataCollectorFactory.collectData;
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
 
 @Data
-public class Template {
+public class Day8 {
 
     private final boolean debug;
 
@@ -98,25 +96,25 @@ public class Template {
     }
 
     public static void main(String[] args) {
-        System.out.println(Template.class);
+        System.out.println(Day8.class);
         long count;
         //*
-        count = new Template(true).solve("day_test.txt");
+        count = new Day8(true).solve("day8_test.txt");
         System.out.println("Result: " + count);
         // add vm option -ea to run configuration to throw exception on assert
         assert count == 11;
 
-        count = new Template(true).solve("day.txt");
+        count = new Day8(true).solve("day8.txt");
         System.out.println("Result: " + count);
         assert count == 22;
 
         //*/
 
-        count = new Template(true).solve2("day_test.txt");
+        count = new Day8(true).solve2("day8_test.txt");
         System.out.println("Result: " + count);
         assert count == 33;
 
-        count = new Template(true).solve2("day.txt");
+        count = new Day8(true).solve2("day8.txt");
         System.out.println("Result: " + count);
         assert count == 44;
         //*/
