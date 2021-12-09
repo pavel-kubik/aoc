2f98304f7cc2be034c91e93b0660c3859333a405
Pavel <roigoci@gmail.com>
2021-12-09 07:25:13 +0100

    Day 9 Part 1

--- src/main/java/cz/pk/adventofcode/current/Day9.java
+++ src/main/java/cz/pk/adventofcode/current/Day9.java
@@ -1,14 +1,12 @@
 package cz.pk.adventofcode.current;
 
 import cz.pk.adventofcode.util.DataCollector;
+import cz.pk.adventofcode.util.Matrix;
 import cz.pk.adventofcode.util.StringCollector;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
@@ -66,57 +64,157 @@ public class Day9 {
         }
     }
 
+    private boolean isMin(List<Integer> vals, Integer value) {
+        for (Integer v : vals) {
+            if (value >= v) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public long solve(String file) {
-        // general data structure
-        //Subject[] data = new TypeCollector(file).process().toArray(new Subject[1]);
-
         // string lines
-        //List<String> data = new StringCollector(file).process();
-
-        // 1 line separated by ,
-        List<Integer> data = stream(new StringCollector(file)
-                .process().get(0).split(",")).map(Integer::valueOf).collect(toList());
-
-        // matrix
-        //List<List<Long>> data = collectData(
-        //        file,
-        //        (line) -> stream(line.split(" ")).map(Long::parseLong).collect(toList()));
-        //Matrix<Long> m = Matrix.instance(data);
-        //m.map(0l, (a, b) -> a + b);
-
+        List<String> in = new StringCollector(file)
+                        .process();
+        List<List<Integer>> mtx = new ArrayList<>();
+        for (String line : in) {
+            mtx.add(line.chars().mapToObj(c -> (char)c).map(c -> Integer.parseInt(c.toString())).collect(toList()));
+        }
+        Matrix<Integer> data = Matrix.instance(mtx);
         System.out.println(data);
-        return data.get(0);
+
+        Matrix<Integer> surround = data.applyOperation(
+                (m, c) -> {
+                    int x = c.first;
+                    int y = c.second;
+                    List<Integer> sur = new ArrayList<>();
+                    for (int i = -1; i < 2; i++) {
+                        for (int j = -1; j < 2; j++) {
+                            if (i==0 && j == 0) continue;
+                            if (m.get(x + i, y + j) != null) {
+                                sur.add(m.get(x + i, y + j));
+                            }
+                        }
+                    }
+                    if (isMin(sur, m.get(x, y))) {
+                        return m.get(x, y);
+                    } else {
+                        return -1;
+                    }
+               });
+        System.out.println(surround);
+
+        int count = surround.map(0, (a, b) -> a + ((b != -1) ? b + 1 : 0));
+
+        return count;
+    }
+
+    private void markBasis(Matrix<Integer> m, int x, int y, int base) {
+        m.set(x, y, -base); // mark as counted
+        for (int dx = -1; dx < 2; dx++) {
+            for (int dy = -1; dy < 2; dy++) {
+                if (dx==0 && dy == 0) continue;
+                if (m.get(x + dx, y + dy) != null && m.get(x + dx, y + dy) == base + 1) {
+                    // new point
+                    if (base + 1 < 9) {
+                        markBasis(m, x + dx, y + dy, base + 1);
+                    }
+                }
+            }
+        }
+        //System.out.printf("(%d,%d): %d\n", x, y, base);
+    }
+
+    private void unmarkBasis(Matrix<Integer> matrix) {
+        matrix.applyOperation((m, c) -> {
+            if (m.get(c.first, c.second) < 0) {
+                m.set(c.first, c.second, -m.get(c.first, c.second));
+            } else {
+                m.set(c.first, c.second, m.get(c.first, c.second));
+            }
+            return 0;
+        });
     }
 
     public long solve2(String file) {
-        List<Integer> data = stream(new StringCollector(file)
-                .process().get(0).split(",")).map(Integer::valueOf).collect(toList());
+        // string lines
+        List<String> in = new StringCollector(file)
+                .process();
+        List<List<Integer>> mtx = new ArrayList<>();
+        for (String line : in) {
+            mtx.add(line.chars().mapToObj(c -> (char)c).map(c -> Integer.parseInt(c.toString())).collect(toList()));
+        }
+        Matrix<Integer> data = Matrix.instance(mtx);
         System.out.println(data);
-        return data.get(0)+22;
+
+        Matrix<Integer> surround = data.applyOperation(
+                (m, c) -> {
+                    int x = c.first;
+                    int y = c.second;
+                    List<Integer> sur = new ArrayList<>();
+                    for (int dx = -1; dx < 2; dx++) {
+                        for (int dy = -1; dy < 2; dy++) {
+                            if (dx==0 && dy == 0) continue;
+                            if (m.get(x + dx, y + dy) != null) {
+                                sur.add(m.get(x + dx, y + dy));
+                            }
+                        }
+                    }
+                    if (isMin(sur, m.get(x, y))) {
+                        int base = m.get(x, y);
+                        System.out.println("Count base " + x + "," + y);
+                        markBasis(m, x, y, base);
+                        //System.out.println(m);
+                        int count = m.map(0, (a, b) -> a + (b < 0 ? 1 : 0));
+                        if (m.get(x, y) == 0) count++;
+                        //System.out.println("Size: " + count);
+                        unmarkBasis(m);
+                        //System.out.println(m);
+                        return count;
+                    } else {
+                        return -1;
+                    }
+                });
+        System.out.println(surround);
+
+        List<Integer> basinsSize = new ArrayList<>();
+        int count = surround.map(0, (a, b) -> {
+            if (b > 0) {
+                basinsSize.add(b);
+            }
+            return 0;
+        });
+
+        System.out.println(basinsSize);
+
+        Collections.sort(basinsSize, Collections.reverseOrder());
+
+        return basinsSize.get(0) * basinsSize.get(1) * basinsSize.get(2);
     }
 
     public static void main(String[] args) {
         System.out.println(Day9.class);
         long count;
-        //*
+        /*
         count = new Day9(true).solve("day9_test.txt");
         System.out.println("Result: " + count);
         // add vm option -ea to run configuration to throw exception on assert
-        assert count == 11;
+        assert count == 15;
 
         count = new Day9(true).solve("day9.txt");
         System.out.println("Result: " + count);
-        assert count == 22;
+        assert count == 545;
 
         //*/
 
         count = new Day9(true).solve2("day9_test.txt");
         System.out.println("Result: " + count);
-        assert count == 33;
+        assert count == 1134;
 
         count = new Day9(true).solve2("day9.txt");
         System.out.println("Result: " + count);
-        assert count == 44;
+        assert count == 44; //>931491
         //*/
     }
 }
