1658784bb8fcf9a6a5c1b2951722b68a8724dfca
Pavel <roigoci@gmail.com>
2021-12-11 06:35:11 +0100

    Day 11 both parts

--- src/main/java/cz/pk/adventofcode/current/Day11.java
+++ src/main/java/cz/pk/adventofcode/current/Day11.java
@@ -1,7 +1,9 @@
 package cz.pk.adventofcode.current;
 
 import cz.pk.adventofcode.util.DataCollector;
+import cz.pk.adventofcode.util.Matrix;
 import cz.pk.adventofcode.util.StringCollector;
+import cz.pk.adventofcode.util.Vector2;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,6 +11,7 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
@@ -52,54 +55,116 @@ public class Day11 {
         }
     }
 
-    class TypeCollector extends DataCollector<Subject> {
+    class TypeCollector extends DataCollector<List<Integer>> {
 
         public TypeCollector(String file) {
             super(file);
         }
 
         @Override
-        protected Subject processLine(String line) {
-            // numbers in line without separator - Subject: List<Integer>
-            //return line.chars().mapToObj(c -> (char)c).map(c -> Integer.parseInt(c.toString())).collect(toList());
-
-            // numbers in line with separator - Subject: List<Integer>
-            // decimal - can be any base - change 2nd argument in parseInt
-            //return stream(line.split(" ")).map(n -> Integer.parseInt(n, 10)).collect(toList());
-
-            Type type = Type.get(String.valueOf(line.charAt(0)));
-            Integer size = Integer.valueOf(line.substring(1));
-            return new Subject(type, size);
+        protected List<Integer> processLine(String line) {
+            return line.chars().mapToObj(c -> (char)c).map(c -> Integer.parseInt(c.toString())).collect(toList());
         }
     }
 
+    private final Vector2<Integer>[] topology = new Vector2[]{
+            new Vector2<>(1, 0),
+            new Vector2<>(-1, 0),
+            new Vector2<>(0, 1),
+            new Vector2<>(0, -1),
+            new Vector2<>(1, 1),
+            new Vector2<>(-1, -1),
+            new Vector2<>(1, -1),
+            new Vector2<>(-1, 1),
+    };
+
+
     public long solve(String file) {
-        // general data structure
-        //Subject[] data = new TypeCollector(file).process().toArray(new Subject[1]);
-
-        // string lines
-        //List<String> data = new StringCollector(file).process();
-
-        // 1 line separated by ,
-        List<Integer> data = stream(new StringCollector(file)
-                .process().get(0).split(",")).map(Integer::valueOf).collect(toList());
-
         // matrix
-        //List<List<Long>> data = collectData(
-        //        file,
-        //        (line) -> stream(line.split(" ")).map(Long::parseLong).collect(toList()));
-        //Matrix<Long> m = Matrix.instance(data);
-        //m.map(0l, (a, b) -> a + b);
+        List<List<Integer>> data = new TypeCollector(file).process();
+        Matrix<Integer> octopusMatrix = Matrix.instance(data);
+        System.out.println(octopusMatrix);
 
-        System.out.println(data);
-        return data.get(0);
+        long flashesSum = 0;
+
+        for (int i = 0; i < 100; i++) {
+            // increase all
+            octopusMatrix.applyOperation((m, idx) -> {
+                m.set(idx, m.get(idx) + 1);
+                return null;
+            });
+            // flashes
+            long flashCountAtStep = 0;
+            do {
+                Matrix<Integer> flashes = octopusMatrix.applyOperation((m, idx) -> {
+                    int flash = 0;
+                    if (m.get(idx) > 9) {
+                        m.set(idx, 0);
+                        flash = 1;
+                        // flash -> increase neighbors
+                        Arrays.stream(topology).forEach(
+                                diff -> {
+                                    Vector2<Integer> field = idx.plus(diff);
+                                    if (m.get(field) != null && m.get(field) > 0) {
+                                        m.set(field, m.get(field) + 1);
+                                    }
+                                }
+                        );
+                    }
+                    return flash;
+                });
+                flashCountAtStep = flashes.map(0, (sum, m) -> sum + m);
+                flashesSum += flashCountAtStep;
+            } while (flashCountAtStep > 0);
+            //System.out.println(octopusMatrix);
+        }
+
+        return flashesSum;
     }
 
     public long solve2(String file) {
-        List<Integer> data = stream(new StringCollector(file)
-                .process().get(0).split(",")).map(Integer::valueOf).collect(toList());
-        System.out.println(data);
-        return data.get(0)+22;
+        // matrix
+        List<List<Integer>> data = new TypeCollector(file).process();
+        Matrix<Integer> octopusMatrix = Matrix.instance(data);
+        System.out.println(octopusMatrix);
+
+        int step = 0;
+        long flashesSum = 0;
+        do {
+            step++;
+            // increase all
+            octopusMatrix.applyOperation((m, idx) -> {
+                m.set(idx, m.get(idx) + 1);
+                return null;
+            });
+            // flashes
+            long flashCountAtStep = 0;
+            flashesSum = 0;
+            do {
+                Matrix<Integer> flashes = octopusMatrix.applyOperation((m, idx) -> {
+                    int flash = 0;
+                    if (m.get(idx) > 9) {
+                        m.set(idx, 0);
+                        flash = 1;
+                        // flash -> increase neighbors
+                        Arrays.stream(topology).forEach(
+                                diff -> {
+                                    Vector2<Integer> field = idx.plus(diff);
+                                    if (m.get(field) != null && m.get(field) > 0) {
+                                        m.set(field, m.get(field) + 1);
+                                    }
+                                }
+                        );
+                    }
+                    return flash;
+                });
+                flashCountAtStep = flashes.map(0, (sum, m) -> sum + m);
+                flashesSum += flashCountAtStep;
+            } while (flashCountAtStep > 0);
+            System.out.println("Step " + step + ": " + flashesSum);
+        } while (flashesSum != octopusMatrix.getWidth()*octopusMatrix.getHeight());
+
+        return step;
     }
 
     public static void main(String[] args) {
@@ -109,17 +174,17 @@ public class Day11 {
         count = new Day11(true).solve("day11_test.txt");
         System.out.println("Result: " + count);
         // add vm option -ea to run configuration to throw exception on assert
-        assert count == 11;
+        assert count == 1656;
 
         count = new Day11(true).solve("day11.txt");
         System.out.println("Result: " + count);
-        assert count == 22;
+        assert count == 1667;
 
         //*/
 
         count = new Day11(true).solve2("day11_test.txt");
         System.out.println("Result: " + count);
-        assert count == 33;
+        assert count == 195;
 
         count = new Day11(true).solve2("day11.txt");
         System.out.println("Result: " + count);
