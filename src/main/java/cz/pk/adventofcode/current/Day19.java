e6468f680d0ad73aeb15797dcca06353480366b0
Pavel <roigoci@gmail.com>
2021-12-20 08:14:35 +0100

    Day 19 part 2 - I will never come back to this code

--- src/main/java/cz/pk/adventofcode/current/Day19.java
+++ src/main/java/cz/pk/adventofcode/current/Day19.java
@@ -1,21 +1,13 @@
 package cz.pk.adventofcode.current;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import cz.pk.adventofcode.util.GroupCollector;
-import cz.pk.adventofcode.util.StringCollector;
 import cz.pk.adventofcode.util.Vector2;
 import cz.pk.adventofcode.util.Vector3;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.nd4j.autodiff.listeners.profiler.data.Phase;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
@@ -154,7 +146,7 @@ public class Day19 {
                 int i = 0;
                 for (Transformation transformation : chain) {
                     out = transformation.transform(out);
-                    System.out.println("Chain " + i++ + ": " + Arrays.toString(out));
+                    //System.out.println("Chain " + i++ + ": " + Arrays.toString(out));
                 }
                 return out;
             } else {
@@ -286,29 +278,21 @@ public class Day19 {
                     }
                     if (probesRelations.containsKey(SEP * i + j)) {
                         Transformation transformationIJ = probesRelations.get(SEP * i + j);
-//                        if (transformationIJ.getChain() != null && z < 2) {
-//                            continue; // don't use chain so much
-//                        }
-                        System.out.println("Extend mapping " + i + " to " + j);
+                        if (debug) System.out.println("Extend mapping " + i + " to " + j);
                         Map<Integer, Transformation> newGen = new HashMap<>();
                         for (Map.Entry<Integer, Transformation> entry : probesRelations.entrySet()) {
                             int fromProbe = entry.getKey() / SEP;
                             int toProbe = entry.getKey() % SEP;
-
-//                            if (entry.getValue().getChain() != null && z < 2) {
-//                                continue; // don't use chain so much
-//                            }
-
                             if (fromProbe == j && toProbe != i) {
                                 int k = toProbe;    // j -> k
                                 // i -> j extend to i -> j -> k
                                 if (!probesRelations.containsKey(SEP * i + k)) {
-                                    System.out.println("Add transformation from " + i + " to " + k);
+                                    if (debug) System.out.println("Add transformation from " + i + " to " + k);
                                     newGen.put(SEP * i + k, transformationIJ.add(entry.getValue()));
                                 }
                                 // i -> j extend to k ->' j ->' i
                                 if (!probesRelations.containsKey(SEP * k + i)) {
-                                    System.out.println("Add transformation from " + k + " to " + i);
+                                    if (debug) System.out.println("Add transformation from " + k + " to " + i);
                                     newGen.put(SEP * k + i, entry.getValue().inverse().add(transformationIJ.inverse()));
                                 }
                             }
@@ -317,24 +301,15 @@ public class Day19 {
                                 // i -> j extend to j ->' i -> k
                                 // 447 when commented out
                                 if (!probesRelations.containsKey(SEP * j + k)) {
-                                    System.out.println("Add transformation from " + j + " to " + k);
+                                    if (debug) System.out.println("Add transformation from " + j + " to " + k);
                                     newGen.put(SEP * j + k, transformationIJ.inverse().add(entry.getValue()));
                                 }
                                 // i -> j extend to k ->' i -> j
                                 if (!probesRelations.containsKey(SEP * k + j)) {
-                                    System.out.println("Add transformation from " + k + " to " + j);
+                                    if (debug) System.out.println("Add transformation from " + k + " to " + j);
                                     newGen.put(SEP * k + j, entry.getValue().inverse().add(transformationIJ));
                                 }
                             }
-                            // new - changed to 483 - WHY???
-//                            if (fromProbe != j && toProbe == i) {
-//                                int k = fromProbe;  // k -> i
-//                                // i -> j extend to k -> i -> j
-//                                if (!probesRelations.containsKey(SEP * k + j)) {
-//                                    System.out.println("Add transformation from " + k + " to " + j);
-//                                    newGen.put(SEP * k + j, entry.getValue().add(transformationIJ));
-//                                }
-//                            }
                         }
                         probesRelations.putAll(newGen);
                     }
@@ -362,10 +337,94 @@ public class Day19 {
     }
 
     public long solve2(String file) {
-        List<Integer> data = stream(new StringCollector(file)
-                .process().get(0).split(",")).map(Integer::valueOf).collect(toList());
-        System.out.println(data);
-        return data.get(0)+22;
+        List<List<int[]>> scannersData = new ScanCollector(file).processGroups();
+        // find subset with offset and changed orientation
+        for (int i = 0; i < scannersData.size(); i++) {
+            for (int j = i + 1; j < scannersData.size(); j++) {
+                List<int[]> scanI = scannersData.get(i);
+                List<int[]> scanJ = scannersData.get(j);
+
+                int[] diff = compareScansWithRotations(i, j, scanI, scanJ);
+                if (diff != null) {
+                    System.out.printf("Scan %d match %d with translation %s\n", i, j, probesRelations.get(SEP*j + i));
+                }
+            }
+        }
+
+        System.out.println(probesRelations);
+
+        // expand probe relations
+        for (int z = 0; z < 3; z++) {
+            for (int i = 0; i < scannersData.size(); i++) {
+                for (int j = 0; j < scannersData.size(); j++) {
+                    if (i == j) {
+                        continue;
+                    }
+                    if (probesRelations.containsKey(SEP * i + j)) {
+                        Transformation transformationIJ = probesRelations.get(SEP * i + j);
+                        Map<Integer, Transformation> newGen = new HashMap<>();
+                        for (Map.Entry<Integer, Transformation> entry : probesRelations.entrySet()) {
+                            int fromProbe = entry.getKey() / SEP;
+                            int toProbe = entry.getKey() % SEP;
+                            if (fromProbe == j && toProbe != i) {
+                                int k = toProbe;    // j -> k
+                                // i -> j extend to i -> j -> k
+                                if (!probesRelations.containsKey(SEP * i + k)) {
+                                    newGen.put(SEP * i + k, transformationIJ.add(entry.getValue()));
+                                }
+                                // i -> j extend to k ->' j ->' i
+                                if (!probesRelations.containsKey(SEP * k + i)) {
+                                    newGen.put(SEP * k + i, entry.getValue().inverse().add(transformationIJ.inverse()));
+                                }
+                            }
+                            if (fromProbe == i && toProbe != j) {
+                                int k = toProbe;    // i -> k
+                                // i -> j extend to j ->' i -> k
+                                // 447 when commented out
+                                if (!probesRelations.containsKey(SEP * j + k)) {
+                                    newGen.put(SEP * j + k, transformationIJ.inverse().add(entry.getValue()));
+                                }
+                                // i -> j extend to k ->' i -> j
+                                if (!probesRelations.containsKey(SEP * k + j)) {
+                                    newGen.put(SEP * k + j, entry.getValue().inverse().add(transformationIJ));
+                                }
+                            }
+                        }
+                        probesRelations.putAll(newGen);
+                    }
+                }
+            }
+        }
+
+        // count Manhattan distance
+        Set<Vector3<Integer>> scanners = new HashSet<>();
+        scanners.add(new Vector3<>(0, 0, 0));
+        for (int scan = 1; scan < scannersData.size(); scan++) {
+            Transformation transformation = probesRelations.get(SEP*scan);
+            if (transformation == null) {
+                System.out.println("Missing transformation " + scan + " to 0");
+            }
+            scanners.add(new Vector3<>(Arrays.stream(
+                    transformation.transform(new int[] {0, 0, 0}))
+                    .boxed()
+                    .toArray(Integer[]::new)));
+
+        }
+        long maxDistance = 0;
+        for (Vector3<Integer> scannerA: scanners) {
+            for (Vector3<Integer> scannerB : scanners) {
+                long currentDistance = manhattanDistance(scannerA, scannerB);
+                if (maxDistance < currentDistance) {
+                    maxDistance = currentDistance;
+                }
+            }
+        }
+
+        return maxDistance;
+    }
+
+    private long manhattanDistance(Vector3<Integer> a, Vector3<Integer> b) {
+        return Math.abs(a.getX() - b.getX()) + Math.abs(a.getY() - b.getY()) + Math.abs(a.getZ() - b.getZ());
     }
 
     public static void main(String[] args) {
@@ -385,11 +444,11 @@ public class Day19 {
 
         count = new Day19(true).solve2("day19_test.txt");
         System.out.println("Result: " + count);
-        assert count == 33;
+        assert count == 3621;
 
         count = new Day19(true).solve2("day19.txt");
         System.out.println("Result: " + count);
-        assert count == 44;
+        assert count == 9668; // > 9561
         //*/
     }
 }
