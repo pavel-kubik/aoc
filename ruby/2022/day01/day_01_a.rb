2594c9de1989adeac87539815b560f3255bdbc32
Pavel <roigoci@gmail.com>
2022-12-01 09:09:01 +0100

    Year 2022 day 1 - ruby

---
+++ ruby/2022/day01/day_01_a.rb
@@ -0,0 +1,16 @@
+file_path = File.expand_path('day_01.txt', __dir__)
+input = File.read(file_path)
+
+calories = Array.new(1, 0)
+elf_index = 0
+
+input.each_line do |line|
+  if line.chomp.empty?
+    elf_index += 1
+    calories.push(0)
+  else
+    calories[elf_index] += line.to_i
+  end
+end
+
+puts calories.max
