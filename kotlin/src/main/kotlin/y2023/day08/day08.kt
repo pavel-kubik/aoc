9383c4bec35c73eb81932c054890fb2a20efda71
Pavel <roigoci@gmail.com>
2023-12-08 21:12:38 +0100

    Speed up with prime factors

--- kotlin/src/main/kotlin/y2023/day08/day08.kt
+++ kotlin/src/main/kotlin/y2023/day08/day08.kt
@@ -2,6 +2,7 @@ package y2023.day08
 
 import com.sun.org.apache.xalan.internal.lib.ExsltMath.power
 import utils.FileReader
+import utils.lcm
 import utils.runWrapper
 import kotlin.math.floor
 import kotlin.math.sqrt
@@ -75,10 +76,44 @@ fun part2(lines: List<String>): Long {
         if (nodeSteps.all { it != -1 }) break
     }
 
-    val step = nodeSteps.maxOf { it }
-    var i = step.toLong()
-    while (!nodeSteps.all { i % it == 0L}) {
-        i += step
+    // https://www.tiger-algebra.com/en/solution/least-common-multiple/lcm%2812%2C18%2C24%2C36%29/#:~:text=The%20least%20common%20multiple%20of,24%20and%2036%20is%2072.
+    val primeNumbersNodes = nodeSteps.map {
+        primeFactors(it)
     }
-    return i
+    val primes = primeNumbersNodes.reduce { acc, i -> acc + i } // jones maps
+    val out = primes.filter { it.value > 0 }.map {
+        power(it.key.toDouble(), it.value.toDouble())
+    }.reduce { acc, d -> acc * d }
+    return out.toLong()
+}
+
+fun primeFactors(input: Int): Map<Int, Int> {
+    var n = input
+    var out = mutableMapOf<Int, Int>()
+    // Print the number of 2s that divide n
+    while (n % 2 == 0) {
+        out.compute(2) { _, v -> if (v == null) 1 else v + 1 };
+        n = floor(n / 2.0).toInt();
+    }
+
+    // n must be odd at this point.
+    // So we can skip one element
+    // (Note i = i +2)
+    //for(let i = 3; i <= Math.floor(Math.sqrt(n)); i = i + 2) {
+    for (i in 3..floor(sqrt(n.toDouble())).toInt() step 2) {
+
+        // While i divides n, print i
+        // and divide n
+        while (n % i == 0) {
+            out.compute(i) { _, v -> if (v == null) 1 else v + 1 };
+            n = floor(n / i.toDouble()).toInt();
+        }
+    }
+
+    // This condition is to handle the
+    // case when n is a prime number
+    // greater than 2
+    if (n > 2)
+        out.compute(n) { _, v -> if (v == null) 1 else v + 1};
+    return out
 }
