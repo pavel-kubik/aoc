0ce7fe0c3a222cb13121a7b9ea352a9b49c732b1
Pavel <roigoci@gmail.com>
2023-12-10 09:19:52 +0100

    Day 10 part 2 not works

--- kotlin/src/main/kotlin/y2023/day10/day10.kt
+++ kotlin/src/main/kotlin/y2023/day10/day10.kt
@@ -7,10 +7,10 @@ import java.util.*
 fun main() {
     val testLines = FileReader.readResourceFile("/day10/test_data.txt")
     val lines = FileReader.readResourceFile("/day10/data.txt")
-    runWrapper { part1(testLines) }
-    runWrapper { part1(lines) }
-    runWrapper { part2(testLines) }
-    runWrapper { part2(lines) }
+    //runWrapper { part1(testLines) }
+    //runWrapper { part1(lines) }
+    runWrapper(8) { part2(testLines) }
+    //runWrapper { part2(lines) }
 }
 
 val UP = Pair(0, -1)
@@ -63,10 +63,10 @@ fun part1(lines: List<String>): Int {
             queue.add(Pair(Pair(newX, newY), node.second + 1))
         }
     }
-    dist.map {
-        it.map { print(" ${if (it == -1) "   ." else it.toString().padStart(4, '0')} ") }
-        println()
-    }
+//    dist.map {
+//        it.map { print(" ${if (it == -1) "   ." else it.toString().padStart(4, '0')} ") }
+//        println()
+//    }
     // remove dead end pipes
     val max = dist.map { it.maxOf { it } }.maxOf { it }
     // 6819 too low
@@ -74,6 +74,89 @@ fun part1(lines: List<String>): Int {
     return max
 }
 
-fun part2(lines: List<String>) {
+fun part2(lines: List<String>): Int {
+    val dim = lines[0].length + 2
+    val map = listOf(".".repeat(dim)) + lines.map { ".$it." } + listOf(".".repeat(dim))
+    //println(map.reduce { acc, s -> "$acc\n$s" })
+    val start = findAnimal(map)
+    val dist = Array(map.size) { Array(dim) {-1} }
+    val queue = LinkedList<Pair<Pair<Int, Int>, Int>>()
+    queue.add(Pair(start, 0))
+    while (queue.isNotEmpty()) {
+        val node = queue.removeFirst()
+        if (dist[node.first.second][node.first.first] != -1) {
+            continue // visited
+        }
+        val (x, y) = node.first
+        dist[y][x] = node.second
+        // add next step
+        val directions = direction[map[y][x]] ?: continue
+        for (d in directions) {
+            val (newX, newY) = Pair(x + d.first, y + d.second)
+            if (dist[newY][newX] != -1) continue
+            if (map[newY][newX] == '.') continue
+            queue.add(Pair(Pair(newX, newY), node.second + 1))
+        }
+    }
+    val queue2 = LinkedList<Pair<Int, Int>>()
+    queue2.add(Pair(1, 1))
+    var iters = 0
+    while (queue2.isNotEmpty()) {
+        val node = queue2.removeLast()
+        val (x, y) = node
+        if (dist[y][x] == -1) {
+            dist[y][x] = -2
+        }
+        for (d in listOf(DOWN, RIGHT, UP, LEFT)) {
+            val (newX, newY) = Pair(x + d.first, y + d.second)
+            if (newX >= 0 && newY >= 0 && newY < dist.size && newX < dist[0].size) {
+                if (dist[newY][newX] != -1) continue
+                queue2.add(Pair(newX, newY))
+            }
+        }
+    }
+    val max = dist.map { it.maxOf { it } }.maxOf { it }
+    println(max)
+    val neigbours = listOf(Pair(-1, -1), Pair(0, -1), Pair(1, -1),
+                            Pair(-1, 0), Pair(1, 0),
+                            Pair(-1, 1), Pair(0, 1), Pair(1, 1))
+    for (i in 0 until dist.size) {
+        for (j in 0 until dist[0].size) {
+            if (dist[i][j] == -1) {
+                if (neigbours.any {
+                        val newX = j + it.first
+                        val newY = i + it.second
+                        if (newX >= 0 && newY >= 0 && newY < dist.size && newX < dist[0].size) {
+                            dist[newY][newX] == -2
+                        } else false
+                }) {
+                    dist[i][j] = -3
+                }
+            }
+        }
+    }
+    // one more removal of fake points inside
+    for (i in 0 until dist.size) {
+        var inside = false
+        for (j in 0 until dist[0].size) {
+            if (listOf('|', 'S', 'L', 'J', 'F', '7').contains(map[i][j])) {
+                inside = !inside
+            }
+            if (dist[i][j] == -1 && !inside) {
+                //dist[i][j] = -4
+            }
+        }
+    }
 
+    dist.map {
+        it.map { print(" ${if (it == -1) "   ." else it.toString().padStart(4, '0')} ") }
+        println()
+    }
+    return dist.map { it.map { if (it == -1) 1 else 0 }.sum() }.sum()// - 2*max
+    // 6524 too high
+    // 630
+    // 586 - 4
+    // 346
+    // 574
+    // 14214 -
 }
